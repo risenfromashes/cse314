0dcbeedad83bb9b9b1e67239b6b2df2d11674fe3
risenfromashes <ashrafurkhan37@gmail.com>
2023-06-19 17:07:31 +0600

    make improvements

--- shell-script/Workspace/organize.sh
+++ shell-script/Workspace/organize.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-
 sub_dir=$1
 target_dir=$2
 test_dir=$3
 ans_dir=$4
+n_test=0
+verbose=false
+exec=true
+exts=("c" "java" "py")
+langs=("C" "Java" "Python")
+filenames=("main.c" "Main.java" "main.py")
+
+for arg in "$@"; do
+  if [[ $arg = "-v" ]]; then
+      verbose=true
+  else if [[ $arg = "-noexecute" ]]; then
+      exec=false
+  fi
+done
 
 if [[ ! -e "$sub_dir" ]]; then
   echo "Error, submission directory doesn't exist"
@@ -15,7 +28,6 @@ if [[ ! -e "$ans_dir" ]]; then
   echo "Error, answer directory doesn't exist"
 fi
 
-# create dirs and file(s)
 if [[ ! -e "$target_dir" ]]; then
   mkdir "$target_dir"
 fi
@@ -26,43 +38,53 @@ for p in "C" "Python" "Java"; do
     mkdir "$f"
   fi
 done
-
 echo "student_id,type,matched,not_matched" > "$target_dir"/result.csv
+
+for test in "$test_dir"/*; do
+  n_test=$((n_test + 1))
+done
+
+if $verbose; then
+  echo Found $n_test test files
+fi
 
 get_lang(){
   case $1 in
-    c)
-      echo "C"
+    c|C)
+      echo 0
       ;;
-    java)
-      echo "Java"
+    java|Java)
+      echo 1
       ;;
-    py)
-      echo "Python"
+    py|Python)
+      echo 2
       ;;
   esac
 }
 
-rename(){
-  name=$(basename "$1")
-  dir=$(dirname "$1")
+compile(){
   case "$2" in
-    c)
-      if [[ "$name" != "main.c" ]]; then
-        mv "$1" "$dir"/main.c
-      fi
+    0)
+      gcc "$1"/main.c -o "$1"/main.out
       ;;
-    py)
-      if [[ "$name" != "main.py" ]]; then
-        mv "$1" "$dir"/main.py
-      fi
-      ;;
-    java)
-      if [[ "$name" != "Main.java" ]]; then
-        mv "$1" "$dir"/Main.java
-      fi
+    1)
+      javac "$1"/Main.java
       ;;
   esac          
+}
+
+run(){
+  case $2 in
+    0)
+      "$1"/main.out < "$3" > "$4"
+      ;;
+    1)
+      java -cp "$1" Main < "$3" > "$4"
+      ;;
+    2)
+      python3 "$1"/main.py < "$3" > "$4"
+      ;;
+  esac
 }
 
 get_test_no(){
@@ -80,14 +102,12 @@ process(){
     ext=${f##*.}
     case "$ext" in 
       c|py|java)
-        lang=$(get_lang $ext)
+        l=$(get_lang $ext)
+        lang=${langs[$l]}
         mkdir "$target_dir"/$lang/$roll
-        cp "$f" "$target_dir"/$lang/$roll
-        rename "$target_dir"/$lang/$roll/* $ext
+        cp "$f" "$target_dir"/$lang/$roll/${filenames[$l]}
         rm -rf "$target_dir"/temp
         break
-        ;;
-      *)
         ;;
     esac
   done
@@ -102,61 +122,24 @@ match(){
   fi
 }
 
-
 for f in "$sub_dir"/*.zip; do
   process "$f"
 done
 
-for d in "$target_dir"/C/*; do
-  gcc "$d"/main.c -o "$d"/main.out
-  matched=0
-  n=0
-  for test in "$test_dir"/*; do
-    i=$(get_test_no "$test")
-    "$d"/main.out < "$test" > "$d"/out$i.txt
-    matched=$((matched + $(match "$d" $i)))
-    n=$((n + 1))
+if $exec; then
+  for d in "$target_dir"/*/*; do
+    roll=${d##*/}
+    lang=${d%/*}
+    lang=${lang##*/}
+    l=$(get_lang $lang)
+    matched=0
+    compile "$d" $l
+    for test in "$test_dir"/*; do
+      i=$(get_test_no "$test")
+      run "$d" $lang "$test" "$d"/out$i.txt
+      matched=$((matched + $(match "$d" $i)))
+    done
+    unmatched=$((n_test - matched))
+    echo $roll, $lang, $matched, $unmatched >> "$target_dir"/result.csv
   done
-  unmatched=$((n - matched))
-done
-
-for d in "$target_dir"/Java/*; do
-  javac "$d"/Main.java
-  matched=0
-  n=0
-  for test in "$test_dir"/*; do
-    i=$(get_test_no "$test")
-    java -cp "$d" Main < "$test" > "$d"/out$i.txt
-    matched=$((matched + $(match "$d" $i)))
-    n=$((n + 1))
-  done
-  unmatched=$((n - matched))
-done
-
-for d in "$target_dir"/Python/*; do
-  matched=0
-  n=0
-  for test in "$test_dir"/*; do
-    i=$(get_test_no "$test")
-    python3 "$d"/main.py < "$test" > "$d"/out$i.txt
-    matched=$((matched + $(match "$d" $i)))
-    n=$((n + 1))
-  done
-  unmatched=$((n - matched))
-done
-
-for d in "$target_dir"/*/*; do
-  roll=${d##*/}
-  lang=${d%/*}
-  lang=${lang##*/}
-  matched=0
-  n=0
-  for test in "$test_dir"/*; do
-    i=$(get_test_no "$test")
-    matched=$((matched + $(match "$d" $i)))
-    n=$((n + 1))
-  done
-  unmatched=$((n - matched))
-  echo $roll, $lang, $matched, $unmatched >> "$target_dir"/result.csv
-done
-  
+fi
